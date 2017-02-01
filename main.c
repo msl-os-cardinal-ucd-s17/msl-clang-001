050a7660c1a437760370ef815dd74f0ed3528d6a
JoshuaMcAllister <joshua.mcallister@ucdenver.edu>
2017-02-01 00:11:10 -0700

    Merged all of our work together. Apologies to Brett for pointer dereferencing confusion (been a while since I coded low-level!) & to David for redundant work.
    Extended out printAndDelete functionality to compensate for dynamically allocated words in node structure.
    Added documentation to source code file, to functions, & to main about usage.
    Fixed memory leaks caused by lack of pointer abstraction in file reading section & in insertNode section.
    Added preprocessor define statements for easy modification of MAX_WORD_LENGTH
    Modified fscanf argument to read preprocessor defined string literal.
    Removed strcpy to utilize move semantics.
    Changed names of some parameters to aid in readability.
    Set const modifer to some function parameters for style purposes (ensures that we don't modify arguments unnecessarily).
    Modified CMake to compile with repository name instead of unrelated name (may need to clean out any previous builds).
    Modified base wordlist.txt to match Stepik assignment.

--- main.c
+++ main.c
@@ -1,37 +1,153 @@
+/* Team Cardinal Programmers:
+ *      Brett Gedvilas
+ *      Joshua McAllister
+ *      Rachel Popo
+ *      David Ward
+ * Date: 1-31-17
+ * Class: CSCI 3453
+ */
+
+/* PROGRAM USAGE:
+ *     Launch the compiled binary with the following command-line argument(s):
+ *          PATH_TO_FILE
+ *
+ *          PATH_TO_FILE: Quotation-mark enclosed string directing to a text file containing the desired words.
+ *              Path may be explicit (e.g. C:\wordlist.txt)
+ *              ...or implicit if the file is located in the same folder as the executable (e.g. .\wordlist.txt)
+ *              The list of words may be space-separated or newline-separated.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
-// NOTE: For now, the number of words & maximum lengths of each word is predefined.
-// TODO: Dynamically reallocate 2D array as file is parsed word-by-word (move semantics instead of copying?!)
-// TODO:   ...and figure out if lengths of each dimension of array are necessary (possibly get away with just checking for null-terminating character?)
+#define MAX_WORD_LENGTH 20
+#define SCANF_STRING_LENGTH_PREP(S) "%" #S "s"
+#define SCANF_STRING_LENGTH_FORMAT(S) SCANF_STRING_LENGTH_PREP(S)
 
-const int NUMBER_OF_WORDS = 7;
-const int MAX_WORD_LENGTH = 20;
+#define ERRNUM_IO 1
+#define ERRNUM_ARGS 2
+#define ERRNUM_HEAP_ALLOCATE_NODE 3
+#define ERRNUM_HEAP_ALLOCATE_WORD 4
 
-//The basic definition of the nodes in the Binary Tree
-typedef struct node {
-    char data[20]; // changed from char* to char []
-    struct node* left;
-    struct node* right;
+typedef struct node str_node;
+
+// The basic definition of the nodes in the Binary Tree
+struct node {
+    char* word;
+    str_node* left;
+    str_node* right;
     int count;
-} str_node;
+};
 
-/* ERROR_FLAG CODES:
- *      0 => No errors.
- *      1 => File I/O errors.
- *      2 => main argument count error.
- *      Specify more when necessary.
+// Global returned by main; 0 if main completed successfully, a defined error code otherwise.
+int errorState = 0;
+
+/* insertNode function documentation
+ * Libraries used: string.h
+ *
+ * This function recursively performs a pre-order traversal through the nodes of the binary tree
+ * and either creates a new node if the word is not found,
+ * or increments the word count if a node is found.
+ *
+ * NOTE: the node needs to be passed by reference in order to correctly assign the
+ *       parent pointer to the new node
+ *
+ * Preconditions:
+ *     inputWord must point to a non-null c-string with 1 or more characters.
+ *     currentNodePtr can point to null or to the root of a subtree.
+ *
+ * Postconditions:
+ *      currentNodePtr points to the root node of a (sub)tree that counted the inputWord.
+ *      insertNode returns 0 if successful, a predefined error constant if failed.
  */
-int ERROR_FLAG = 0;
 
-// Note, for our purpose there aren't any exceptions that we could handle that I can think
-// of except SEGFAULT, which we have been careful to check for,
-// so the current error handling seems to suffice...
+int insertNode(char* inputWord, str_node** currentNodePtr) {
+    assert(currentNodePtr != NULL);
+    int comparison;
 
-char** readFile(char* fileLocation) {
+    if ((*currentNodePtr) == NULL) {
+        // we have reached the end of a tree branch which means a new word needs to be inserted
+        // Allocate memory for the new node and assign the location to leafptr
+
+        (*currentNodePtr) = (str_node*)malloc(sizeof(str_node));
+        if ((*currentNodePtr) == NULL) {
+            return ERRNUM_HEAP_ALLOCATE_NODE;
+        }
+
+        //Set the node pointers to NULL since we followed the paradigm of adding new nodes beyond the leaf without rearranging
+        (*currentNodePtr)->left = NULL;
+        (*currentNodePtr)->right = NULL;
+
+        (*currentNodePtr)->word = inputWord;
+
+        //Set the count to an intial value
+        (*currentNodePtr)->count = 1;
+    }
+
+        // Otherwise, compare value to the current node
+    else {
+        comparison = strcmp(inputWord, (*currentNodePtr)->word);
+
+        if (comparison == 0) {
+            // if words are the same, increment count
+            ((*currentNodePtr)->count)++;
+        }
+        else if (comparison < 0) {
+            // enter the left subtree
+            insertNode(inputWord, &((*currentNodePtr)->left));
+        }
+        else {
+            // enter the right subtree
+            insertNode(inputWord, &((*currentNodePtr)->right));
+        }
+    }
+
+    return 0;
+}
+
+// This function prints the tree and frees all the memory through a post-order traversal.
+void printAndDelete(str_node **node_) {
+    // Try to visit left branch
+    if ((*node_)->left != NULL) {
+        printAndDelete(&((*node_)->left));
+    }
+
+    // Try to visit right branch
+    if ((*node_)->right != NULL) {
+        printAndDelete(&((*node_)->right));
+    }
+
+    // Print the word and word count
+    printf("%s %d\n", (*node_)->word, (*node_)->count);
+
+    // Free memory
+    free((*node_)->word);
+    free(*node_);
+
+}
+
+/* Reads a file from a provided pointer to a c-string of a file path
+ * and constructs a binary tree one word at a time (expected run-time of O(n*lg(n)))
+ *
+ * Preconditions:
+ *      fileLocation cannot be NULL & it must point to an initialized c-string.
+ *
+ * Postconditions:
+ *      If the file could not be read,
+ *          returns NULL and sets the global errorState variable to ERRNUM_IO.
+ *      If the file could be read but it began with a terminating character,
+ *          returns NULL while errorState is 0.
+ *      If the file could be read and valid words exist & precede any invalid words,
+ *          returns root pointer to lexicographically sorted binary tree.
+ */
+
+str_node* createBinaryTreeFromFile(const char** fileLocation) {
+    assert((fileLocation != NULL) && ((*fileLocation) != NULL));
     FILE* fp = NULL;
-    char**wordArray = NULL;
+    str_node* root = NULL;
+    char* currentWord = NULL;
 
     // NOTE: fopen looks for the file in one of two ways:
     //      1) An explicit path to the file (e.g. "C:\wordlist.txt")
@@ -39,192 +155,78 @@ char** readFile(char* fileLocation) {
     //      You will need to copy the supplied wordlist.txt file to wherever it is convenient
     //            (either in your clone of the cmake-build folder for an implicit search,
     //             or to anywhere else for an explicit search).
-    fp = fopen(fileLocation, "r");
+    fp = fopen((*fileLocation), "r");
 
-    //This checks to make sure the file was successfully loaded (fopen() returns a NULL pointer if unsuccessful)
+    // This checks to make sure the file was successfully loaded (fopen() returns a NULL pointer if unsuccessful)
     if (fp == NULL) {
+        errorState = ERRNUM_IO;
         return NULL;
     }
 
-    // TODO: Potentially replace with single-line malloc/free & use pointer arithmetic for navigation.
-    // TODO: Determine the effects of having a 2D array allocated as such being utilized in subscript format in other functions (is it even possible?)
-    wordArray = (char **) malloc(NUMBER_OF_WORDS * sizeof(char *));
-    for (int i = 0; i < NUMBER_OF_WORDS; ++i) {
-        wordArray[i] = (char *) malloc(MAX_WORD_LENGTH * sizeof(char));
-    }
+    while (!feof(fp)) {
+        // Space allocated for additional null-character element
+        currentWord = (char*)malloc(sizeof(char) * (MAX_WORD_LENGTH+1));
 
-    int i = 0;
-    while (!feof(fp) && i < NUMBER_OF_WORDS) {
-        if (fscanf(fp, "%s", wordArray[i]) != 1) {
+        if (currentWord == NULL) {
+            errorState = ERRNUM_HEAP_ALLOCATE_WORD;
             break;
         }
-        ++i;
+
+        if (fscanf(fp, SCANF_STRING_LENGTH_FORMAT(MAX_WORD_LENGTH), currentWord) != 1) {
+            free(currentWord);
+            break;
+        }
+
+        if (insertNode(currentWord, &root) != 0) {
+            errorState = ERRNUM_HEAP_ALLOCATE_NODE;
+            break;
+        }
     }
 
     fclose(fp);
-    return wordArray;
+    return root;
 }
 
-//Takes argument of the input string and the root node of a tree/subtree
-void insertNode(char* word_, str_node** node_) {
-
-    /*
-     * This function recursively visits the nodes of the binary tree and either creates
-     * a new node if the word is not found, or increments the word count if a node is
-     * found.
-     *
-     * NOTE: the node needs to be passed by reference in order to correctly assign the
-     *       parent pointer to the new node
-     */
-
-    int comparison;
-
-
-    if (*node_ == NULL) {
-        // we have reached the end of a tree branch which means a new word needs to be
-        // inserted
-        // Allocate memory for the new node and assign the location to leafptr
-
-        // Dynamically declare a new node
-        str_node *tmpnode = NULL;
-
-        tmpnode = malloc(sizeof(str_node));
-
-        //Copy the string data instead of implicit sharing or move semantics...
-        strcpy(tmpnode->data, word_);
-
-        //Set the node pointers to NULl since we followed the paradigm of adding new nodes beyond the leaf without rearranging
-        tmpnode->left = NULL;
-        tmpnode->right = NULL;
-
-        //Set the count to an intial value
-        tmpnode->count = 1;
-
-        // Allow the parent node to point to the newly created node
-        *node_ = tmpnode;
-    }
-
-    // Otherwise, compare value to the current node
-    else {
-
-        comparison = strcmp(word_, (*node_)->data);
-
-        if (comparison == 0) {
-            // if words are the same, increment count
-            (*node_)->count++;
-        }
-        else if (comparison < 0) {
-            // enter the left subtree
-            insertNode(word_, &(*node_)->left);
-        }
-        else {
-            // enter the right subtree
-            insertNode(word_, &(*node_)->right);
-        }
-    }
-
-    return;
-}
-
-
-void printAndDelete(str_node **node_) {
-    /*
-     * This function prints the tree and frees all the memory
-     */
-
-    // Try to visit left branch
-    if ((*node_)->left != NULL) {
-        printAndDelete(&(*node_)->left);
-    }
-
-    // Try to visit right branch
-    if ((*node_)->right != NULL) {
-        printAndDelete(&(*node_)->right);
-    }
-
-    // Print the word and word count
-    printf("%s %d\n", (*node_)->data, (*node_)->count);
-
-    // Free memory
-    free(*node_);
-
-}
-
-
-int main(int argc, char *argv[]) {
-    printf("Number of arguments: %d\n", argc);
+int main(int argc, const char *argv[]) {
+    printf("Number of user-specified command-line arguments: %d\n\n", (argc-1));
     if (argc != 2) {
         printf("ERROR: Illegal number of arguments.\n");
-        exit(2);
+        printf("COMMAND-LINE ARGUMENTS:\n"
+                       "PATH_TO_FILE\n\n"
+                       "PATH_TO_FILE: Quotation-mark enclosed string directing to a text file containing the desired words.\n"
+                       "\tPath may be explicit (e.g. \"C:\\wordlist.txt\")\n"
+                       "\t...or implicit if text file located in same folder as executable (e.g. \".\\wordlist.txt\")");
+        exit(ERRNUM_ARGS);
     }
 
-//=======================================================
+    printf("Now constructing binary tree from provided list of words...\n");
+    str_node* root = NULL;
+    root = createBinaryTreeFromFile(&(argv[1]));
 
-    /*
-     * I just added this loop in to read the file and test out the insertNode function. I
-     * wasn't sure of the best approach to integrating the readFile() function with what I was
-     * trying to implement. This appears to at least show that the program is working correctly
-     * though.
-     */
-
-    // Open the file
-    char *fileLocation = argv[1];
-    FILE* fp = NULL;
-    fp = fopen(fileLocation, "r");
-
-    char testWord[MAX_WORD_LENGTH];
-    str_node *rootNode = NULL;
-
-    size_t length; // allocate variable for the length of each word
-
-    while(fgets(testWord, MAX_WORD_LENGTH, fp) != NULL) {
-
-        // get length of string
-        length = strlen(testWord);
-        testWord[length - 1] = '\0'; // remove newline from the string
-
-        // Insert the new word into the tree (or increment count if it is already present)
-        insertNode(testWord, &rootNode);
+    if (root == NULL) {
+        if (errorState == ERRNUM_IO) {
+            printf("I/O ERROR: Cannot read provided file path.\n");
+        }
+        else {
+            printf("ERROR: File is empty or begins with illegal characters.");
+        }
+        exit(errorState);
     }
+
+    if (errorState == ERRNUM_HEAP_ALLOCATE_NODE) {
+        printf("WARNING: Not enough memory on heap to allocate another node of the binary tree.\n");
+    }
+
+    if (errorState == ERRNUM_HEAP_ALLOCATE_WORD) {
+        printf("WARNING: Not enough memory on heap to allocate another word;\n"
+                       "\tfile may have unread words.\n");
+    }
+
+    printf("Construction of binary tree finished.\n"
+                   "\nNow printing each word of the tree & its count (post-order traversal):\n");
 
     // Print tree and delete dynamically allocated memory
-    printAndDelete(&rootNode);
+    printAndDelete(&root);
 
-//=======================================================
-
-
-//    char** returnedArray = NULL;
-//    returnedArray = readFile(argv[1]);
-//
-//    if (returnedArray != NULL) {
-//        // Check to see that returnedArray was properly returned.
-//        for (int i = 0; i < NUMBER_OF_WORDS; ++i) {
-//            if (returnedArray[i] != NULL) {
-//                printf("%s\n", returnedArray[i]);
-//            }
-//        }
-//
-//        // INSERT ARRAY PARSING FUNCTIONALITY HERE (use predefined globals for now to determine each dimension's length, if necessary)
-//    } else {
-//        ERROR_FLAG = 1;
-//    }
-//
-//    if (ERROR_FLAG == 1) {
-//        printf("ERROR: %s could not be found.", argv[1]);
-//    }
-//
-//    // TODO: Replace with the TODO's noted in the readFile function, if necessary for optimization.
-//    if (returnedArray != NULL) {
-//        for (int i = 0; i < NUMBER_OF_WORDS; ++i) {
-//            if (returnedArray[i] != NULL) {
-//                free(returnedArray[i]);
-//            }
-//        }
-//        free(returnedArray);
-//    }
-//
-//    return ERROR_FLAG;
-
+    return errorState;
 }
-
-// TODO: Determine the proper place to free memory...
